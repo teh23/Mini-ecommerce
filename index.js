359e776603b08072b53e6738279f044e253e2221
Andrzej Franek <andrzejfranek@hotmail.com>
2021-10-06 22:55:56 +0200

    fix req miss router

--- index.js
+++ index.js
@@ -8,15 +8,15 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 app.use(cors());
 app.use(express.static(path.resolve(__dirname, "./client/build")));
-app.get("*", (request, response) => {
-    response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
-});
 
 app.get("/api", (req, res) => {
     console.log("log");
     res.send("hiho");
 });
 
+app.get("*", (request, response) => {
+    response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });

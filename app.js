eb17bc193fbff1650df13429ba1854e494be4e24
Andrzej Franek <andrzejfranek@hotmail.com>
2021-10-07 23:11:08 +0200

    body parser

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ const WebSocket = require("./websocket");
 const routes = require("./routes");
 const app = express();
 const data = require("./globals").data;
-
+const bodyParser = require("body-parser");
 WebSocket(data);
 
 app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(bodyParser.json());
 app.use(express.static(path.resolve(__dirname, "./client/build")));
 
 app.use("/api", routes);

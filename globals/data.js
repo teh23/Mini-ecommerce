abcfc5b03f8031182ab3d589d6e2bdb0f1b0c68d
Andrzej Franek <andrzejfranek@hotmail.com>
2021-10-08 14:25:18 +0200

    data change & rest values

--- globals/data.js
+++ globals/data.js
@@ -1,15 +1,3 @@
-class Data {
-    constructor() {
-        this.data = [];
-    }
-
-    push(obj) {
-        data.push(obj);
-    }
-
-    shift() {
-        return data.shift();
-    }
-}
+const Data = [];
 
 module.exports = Data;

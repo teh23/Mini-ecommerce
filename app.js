336ea5313d12f1ee534f6d31aa9f8285de9477df
Andrzej Franek <andrzejfranek@hotmail.com>
2021-10-08 19:04:10 +0200

    fix

--- app.js
+++ app.js
@@ -13,12 +13,12 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(bodyParser.json());
-app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 app.use("/api", routes);
 
 app.get("*", (request, response) => {
-    response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+    response.sendFile(path.join(__dirname, "./client/build", "index.html"));
 });
 
 module.exports = app;
